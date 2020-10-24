99475abd4f26fb1b42d88df00bc8d346bfb1acd3
takapy <takanobu.030210@gmail.com>
2020-10-24 13:56:27 +0900

    #26 configクラスの作成&rankgaussの追加

--- snippet/feature-engineering.py
+++ snippet/feature-engineering.py
@@ -1,12 +1,47 @@
 import pandas as pd
-from sklearn.preprocessing import StandardScaler, RobustScaler, MinMaxScaler, OneHotEncoder
+from sklearn.preprocessing import StandardScaler, RobustScaler, MinMaxScaler, OneHotEncoder, QuantileTransformer
 from sklearn.feature_selection import VarianceThreshold
 from sklearn.decomposition import PCA, TruncatedSVD
 from sklearn.cluster import KMeans
 
 
+class Config:
+    # setting
+    is_debug = False
+    is_kaggle = False
+    is_pretrain = False
+
+    # features
+    do_variancethreshold = False
+    do_kmeans = False
+    do_filter = True
+    do_feature_squared = True
+    do_feature_stats = True
+    do_feature_pca = True
+    do_feature_svd = True
+    do_feature_fa = True
+
+    # constant
+    seed = 42
+    n_gene_comp = 70
+    n_cell_comp = 10
+    n_gene_kmeans_cluster = 30
+    n_cell_kmeans_cluster = 5
+    n_variance_threshold = 0.7
+    scaler = 'Rankgauss'  # Standard, Robust, MinMax
+
+    # HyperParameters
+    epochs = 80
+    seed_avg = [0, 101, 202, 303, 404, 999]
+    nfold = 7
+    verbose = 0
+    lr = 1e-3
+    weight_decay = 1e-5
+    batch_size = 128
+
+
 DATA_DIR = '../input/lish-moa/'
-SEED = 42
+config = Config()
 
 
 def data_filter(train, test):
@@ -16,7 +51,6 @@ def data_filter(train, test):
     test = test[test['cp_type']!='ctl_vehicle'].reset_index(drop=True)
     train = train.drop('cp_type', axis=1)
     test = test.drop('cp_type', axis=1)
-
     return train, test
 
 
@@ -30,11 +64,10 @@ def one_hot_encoder(df, cols):
         df = pd.concat([df, ohe_df], axis=1)
         # oheしたカラムを除外
         df = df.drop(col, axis=1)
-
     return df
 
 
-def get_kmeans_label(df, n_cluster):
+def kmeans(df, n_cluster):
     """k-meansで教師なし学習（クラスタ分類）
     """
     km = KMeans(
@@ -67,33 +100,61 @@ def feature_stats(df):
     df.loc[:, 'gc-std'] = df[GENES + CELLS].std(axis=1)
     df.loc[:, 'gc-kurt'] = df[GENES + CELLS].kurtosis(axis=1)
     df.loc[:, 'gc-skew'] = df[GENES + CELLS].skew(axis=1)
-
     return df
 
 
-def feature_pca(df, col_list, n_comp, col_type):
+def feature_pca(df, col_list, n_comp, col_type='g', seed=config.seed):
     """PCAの特徴量を生成
     """
-    pca = (PCA(n_components=n_comp, random_state=SEED).fit_transform(df[col_list]))
+    pca = (PCA(n_components=n_comp, random_state=seed).fit_transform(df[col_list]))
     pca_df = pd.DataFrame(pca, columns=[f'{col_type}-pca_{i}' for i in range(n_comp)])
     df = pd.concat([df, pca_df], axis=1)
     return df
 
 
-def feature_svd(df, col_list, n_comp, col_type):
+def feature_svd(df, col_list, n_comp, col_type='g', seed=config.seed):
     """SVDの特徴量を生成
     """
-    svd = (TruncatedSVD(n_components=n_comp, random_state=SEED).fit_transform(df[col_list]))
+    svd = (TruncatedSVD(n_components=n_comp, random_state=seed).fit_transform(df[col_list]))
     svd_df = pd.DataFrame(svd, columns=[f'{col_type}-svd_{i}' for i in range(n_comp)])
     df = pd.concat([df, svd_df], axis=1)
     return df
 
 
-def variance_threshold(df, n=0.4):
+def feature_fa(df, col_list, n_comp, col_type='g', seed=config.seed):
+    """FAの特徴量を生成
+    """
+    svd = (FactorAnalysis(n_components=n_comp, random_state=seed).fit_transform(df[col_list]))
+    svd_df = pd.DataFrame(svd, columns=[f'{col_type}-fa_{i}' for i in range(n_comp)])
+    df = pd.concat([df, svd_df], axis=1)
+    return df
+
+
+def feature_squared(df, cols_list):
+    """二乗を計算
+    """
+    for feature in cols_list:
+        df.loc[:, f'{feature}_squared'] = df[feature] ** 2
+    return df
+
+
+def variance_threshold(df, n):
     """分散がしきい値以下の特徴量を捨てる
     """
     var_thresh = VarianceThreshold(threshold=n)
     df = pd.DataFrame(var_thresh.fit_transform(df))
+    return df
+
+
+def rankgauss(df, cols, seed=config.seed):
+    """RankGauss
+    """
+    for col in cols:
+        transformer = QuantileTransformer(n_quantiles=100, random_state=seed, output_distribution="normal")
+        vec_len = len(df[col].values)
+        raw_vec = df[col].values.reshape(vec_len, 1)
+        transformer.fit(raw_vec)
+        df[col] = transformer.transform(raw_vec).reshape(1, vec_len)[0]
     return df
 
 
@@ -105,51 +166,80 @@ def feature_engineering(train_features, test_features):
     GENES = [col for col in train_features.columns if col.startswith('g-')]
     CELLS = [col for col in train_features.columns if col.startswith('c-')]
 
-    # filter
-    train, test = data_filter(train_features, test_features)
+    # カテゴリカラム
+    cat_columns = ['cp_time', 'cp_dose']
 
-    # 結合
+    # filter
+    if config.do_filter:
+        print('do filter')
+        train, test = data_filter(train_features, test_features)
+
     df = pd.concat([train, test])
     df = df.reset_index(drop=True)
 
     # k-means cluster
-    df.loc[:, 'g-cluster'] = get_kmeans_label(df[GENES], n_cluster=35)
-    df.loc[:, 'c-cluster'] = get_kmeans_label(df[CELLS], n_cluster=5)
+    if config.do_kmeans:
+        print('do k-means')
+        df.loc[:, 'g-cluster'] = kmeans(df[GENES], n_cluster=config.n_gene_kmeans_cluster)
+        df.loc[:, 'c-cluster'] = kmeans(df[CELLS], n_cluster=config.n_cell_kmeans_cluster)
+        cat_columns = cat_columns + ['g-cluster', 'c-cluster']
 
-    # atats feature
-    df = feature_stats(df)
+    # Stats feature
+    if config.do_feature_stats:
+        print('do feature_stats')
+        df = feature_stats(df)
+
+    # squared
+    if config.do_feature_squared:
+        print('do feature_squared')
+        df = feature_squared(df, CELLS)
 
     # PCA feature
-    df = feature_pca(df, GENES, n_comp=30, col_type='g')
-    df = feature_pca(df, CELLS, n_comp=5, col_type='c')
+    if config.do_feature_pca:
+        print('do feature_pca')
+        df = feature_pca(df, GENES, n_comp=config.n_gene_comp, col_type='g')
+        df = feature_pca(df, CELLS, n_comp=config.n_cell_comp, col_type='c')
 
     # SVD feature
-    df = feature_svd(df, GENES, n_comp=30, col_type='g')
-    df = feature_svd(df, CELLS, n_comp=5, col_type='c')
+    if config.do_feature_svd:
+        print('do feature_svd')
+        df = feature_svd(df, GENES, n_comp=config.n_gene_comp, col_type='g')
+        df = feature_svd(df, CELLS, n_comp=config.n_cell_comp, col_type='c')
 
-    # カテゴリDFとnotカテゴリDFに分割（標準化&エンコードのため）
-    cat_columns = ['cp_time', 'cp_dose', 'g-cluster', 'c-cluster']
+    # FA feature
+    if config.do_feature_fa:
+        print('do feature_fa')
+        df = feature_fa(df, GENES, n_comp=config.n_gene_comp, col_type='g')
+        df = feature_fa(df, CELLS, n_comp=config.n_cell_comp, col_type='c')
+
+    # カテゴリのDFとnotカテゴリのDFに分割（標準化&エンコードのため）
     cat_df = df[['sig_id'] + cat_columns]
     num_df = df.drop(['sig_id'] + cat_columns, axis=1)
 
     # VarianceThreshold
-    num_df = variance_threshold(num_df, n=0.4)
+    if config.do_variancethreshold:
+        print('do variancethreshold')
+        num_df = variance_threshold(num_df, n=config.n_variance_threshold)
 
-    # notカテゴリDFを正規化
-    sscaler = StandardScaler()
-    num_df.iloc[:, :] = sscaler.fit_transform(num_df)
+    # 正規化
+    if config.scaler == 'Rankgauss':
+        print('do Rankgauss')
+        df = rankgauss(df, num_df.columns.tolist())
 
-    # Robust Scaler
-    """
-    rscaler = RobustScaler()
-    num_df.iloc[:, :] = rscaler.fit_transform(num_df)
-    """
+    elif config.scaler == 'Standard':
+        print('do Standard')
+        sscaler = StandardScaler()
+        num_df.iloc[:, :] = sscaler.fit_transform(num_df)
 
-    # min max Scaler
-    """
-    mmscaler = MinMaxScaler()
-    num_df.iloc[:, :] = mmscaler.fit_transform(num_df)
-    """
+    elif config.scaler == 'Robust':
+        print('do Robust')
+        rscaler = RobustScaler()
+        num_df.iloc[:, :] = rscaler.fit_transform(num_df)
+
+    elif config.scaler == 'MinMax':
+        print('do MinMax')
+        mmscaler = MinMaxScaler()
+        num_df.iloc[:, :] = mmscaler.fit_transform(num_df)
 
     # カテゴリ変数をone-hot-encode
     cat_df = one_hot_encoder(cat_df, cat_columns)
